962612f5ad4b6faeeece128c2941219ffbb62fe2
losbiw <skorinov41@gmail.com>
2021-11-07 18:26:55 +0200

    group up styles

--- features/introduction/nav.tsx
+++ features/introduction/nav.tsx
@@ -5,7 +5,9 @@ import colors from '../../constants/colors';
 import {RootState} from '../../store';
 import Gradient from '../general/gradient';
 import SText from '../general/text';
+import WideButton from '../general/wide-button';
 import {hideIntroduction, setSlideIndex, showNextSlide} from './introSlice';
+import slides from './slides-data';
 
 export type SlideIndex = number;
 type SetSlideIndex = (index: number) => void;
@@ -17,16 +19,15 @@ const renderBalls = (
 ) =>
   new Array(count).fill(null).map((_el, index) => (
     <View
-      style={Object.assign(
-        {},
-        styles.container,
+      style={[
+        circles.container,
         index === count - 1 ? {} : styles.margin,
         index === focusedIndex ? styles.opacity : {},
-      )}
+      ]}
       key={`cirlce-${index}`}>
       <Gradient style={styles.borderRadius}>
-        <Pressable onPress={() => setIndex(index)} style={styles.button}>
-          {index === focusedIndex && <View style={styles.circle} />}
+        <Pressable onPress={() => setIndex(index)} style={circles.circle}>
+          {index === focusedIndex && <View style={circles.innerCircle} />}
         </Pressable>
       </Gradient>
     </View>
@@ -36,13 +37,23 @@ const Nav: FC = () => {
   const activeIndex = useSelector((store: RootState) => store.intro.slideIndex);
   const dispatch = useDispatch();
 
+  if (activeIndex === slides.length - 1) {
+    return (
+      <WideButton
+        style={styles.wideButton}
+        onPress={() => dispatch(hideIntroduction())}
+        title="Get Started"
+      />
+    );
+  }
+
   return (
     <View style={styles.nav}>
       <Pressable onPress={() => dispatch(hideIntroduction())}>
         <SText style={styles.pressable}>Skip</SText>
       </Pressable>
 
-      <View style={styles.circles}>
+      <View style={styles.row}>
         {renderBalls(3, activeIndex, (index: number) =>
           dispatch(setSlideIndex(index)),
         )}
@@ -55,35 +66,41 @@ const Nav: FC = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  nav: {
-    paddingHorizontal: 33,
-    width: '100%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  circles: {
-    flexDirection: 'row',
-  },
+const circles = StyleSheet.create({
   container: {
     overflow: 'hidden',
     opacity: 0.5,
   },
-  button: {
+  circle: {
     width: 20,
     height: 20,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  borderRadius: {
-    borderRadius: 10,
-  },
-  circle: {
+  innerCircle: {
     width: 6,
     height: 6,
     backgroundColor: colors.background,
     borderRadius: 4,
+  },
+});
+
+const styles = StyleSheet.create({
+  nav: {
+    paddingHorizontal: 63,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  wideButton: {
+    marginHorizontal: 33,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  borderRadius: {
+    borderRadius: 10,
   },
   opacity: {
     opacity: 1,
