95d0e8c1e9849b566bf449686d02f790c69af6da
losbiw <skorinov41@gmail.com>
2021-11-08 03:10:22 +0200

    add previous slide reducer

--- features/introduction/introSlice.ts
+++ features/introduction/introSlice.ts
@@ -19,10 +19,13 @@ const introSlice = createSlice({
       state.isShown = false;
     },
     showNextSlide: (state: State) => {
-      if (state.slideIndex + 1 >= slides.length) {
-        state.slideIndex = 0;
-      } else {
+      if (state.slideIndex + 1 < slides.length) {
         state.slideIndex += 1;
+      }
+    },
+    showPrevSlide: (state: State) => {
+      if (state.slideIndex - 1 >= 0) {
+        state.slideIndex -= 1;
       }
     },
     setSlideIndex: (state: State, action: PayloadAction<number>) => {
@@ -31,7 +34,7 @@ const introSlice = createSlice({
   },
 });
 
-export const {hideIntroduction, showNextSlide, setSlideIndex} =
+export const {hideIntroduction, showNextSlide, showPrevSlide, setSlideIndex} =
   introSlice.actions;
 
 export default introSlice.reducer;
