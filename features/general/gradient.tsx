468812a138db73b76a287aa8109ae1ca314bbdb4
losbiw <skorinov41@gmail.com>
2021-11-16 23:13:03 +0200

    gradient component can accept custom colors

--- features/general/gradient.tsx
+++ features/general/gradient.tsx
@@ -1,18 +1,31 @@
 import React, {FC} from 'react';
-import {ViewProps} from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
-import colors from '../../constants/colors';
+import LinearGradient, {
+  LinearGradientProps,
+} from 'react-native-linear-gradient';
+import colorPalette from '../../constants/colors';
 import {hextToRGBString} from '../../helpers/hexToRGB';
 
-const Gradient: FC<ViewProps> = props => (
-  <LinearGradient
-    {...props}
-    start={{x: 0, y: 0}}
-    end={{x: 1, y: 1}}
-    angleCenter={{x: 0.5, y: 0.5}}
-    colors={[hextToRGBString(colors.accent1), hextToRGBString(colors.accent2)]}>
-    {props.children}
-  </LinearGradient>
-);
+type Props = Partial<LinearGradientProps>;
+
+const Gradient: FC<Props> = props => {
+  const gradientColors = props.colors
+    ? props.colors
+    : [colorPalette.accent1, colorPalette.accent2];
+
+  const convertedColors = gradientColors.map(color =>
+    typeof color === 'string' ? hextToRGBString(color) : color,
+  );
+
+  return (
+    <LinearGradient
+      {...props}
+      start={{x: 0, y: 0}}
+      end={{x: 1, y: 1}}
+      angleCenter={{x: 0.5, y: 0.5}}
+      colors={convertedColors}>
+      {props.children}
+    </LinearGradient>
+  );
+};
 
 export default Gradient;
