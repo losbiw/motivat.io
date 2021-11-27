52e82903335f34b62fc3cdf08d8bb9453857b469
losbiw <skorinov41@gmail.com>
2021-11-27 20:45:40 +0200

    add filter component

--- features/goals/search/filter.tsx
+++ features/goals/search/filter.tsx
@@ -1,6 +1,25 @@
-import {FC} from 'react';
-import {View} from 'react-native';
+import React, {FC} from 'react';
+import {StyleSheet, View} from 'react-native';
+import colors from '../../../constants/colors';
 
-const Filter: FC = () => <View />;
+const Filter: FC = () => (
+  <View style={styles.container}>
+    <View style={styles.icon} />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 12,
+    borderWidth: 1,
+    borderColor: colors.border,
+    borderRadius: 15,
+    marginLeft: 6,
+  },
+  icon: {
+    width: 26,
+    height: 26,
+  },
+});
 
 export default Filter;
---
+++ features/goals/search/search.tsx
@@ -0,0 +1,19 @@
+import React, {FC} from 'react';
+import {StyleSheet, View} from 'react-native';
+import Filter from './filter';
+import SearchBar from './search-bar';
+
+const Search: FC = () => (
+  <View style={styles.container}>
+    <SearchBar />
+    <Filter />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+});
+
+export default Search;
