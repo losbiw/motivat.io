cfd75fc419dd4f1a84e9c0564eeb919e0050cdb4
losbiw <skorinov41@gmail.com>
2021-11-27 20:46:05 +0200

    add search bar icon

--- features/goals/search/search-bar.tsx
+++ features/goals/search/search-bar.tsx
@@ -1,11 +1,17 @@
 import React, {FC} from 'react';
 import {StyleSheet, View} from 'react-native';
 import colors from '../../../constants/colors';
+import Icon from '../../general/icon';
 import SText from '../../general/text';
 
 const SearchBar: FC = () => {
   return (
     <View style={styles.background}>
+      <Icon
+        style={styles.image}
+        color={colors.placeholder}
+        source={require('../../../assets/images/search/search.png')}
+      />
       <SText style={styles.placeholder}>Search goals</SText>
     </View>
   );
@@ -13,6 +19,9 @@ const SearchBar: FC = () => {
 
 const styles = StyleSheet.create({
   background: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
     paddingVertical: 13,
     paddingHorizontal: 17,
     borderWidth: 1,
@@ -22,6 +31,11 @@ const styles = StyleSheet.create({
   placeholder: {
     fontSize: 16,
     color: colors.placeholder,
+    marginLeft: 16,
+  },
+  image: {
+    width: 20,
+    height: 20,
   },
 });
 
