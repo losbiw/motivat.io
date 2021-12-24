fc38d12d0edb3c231fd64e1ff36fdb5cf55d97c5
losbiw <skorinov41@gmail.com>
2021-12-24 20:06:57 +0200

    add basic settings styles

--- features/settings/settings.tsx
+++ features/settings/settings.tsx
@@ -1,12 +1,35 @@
-import {FC} from 'react';
-import {Text, View} from 'react-native';
+import React, {FC} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import { useSelector } from 'react-redux';
+import {Goal} from '../../types/goal';
+import {State} from './settings-slice';
 
-const Settings: FC = () => {
+type Props = Pick<State, 'id'>;
+
+const Settings: FC<Props> = props => {
+  // const itemId = useSelector();
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Settings</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    alignItems: 'center',
+    paddingHorizontal: 33,
+    paddingTop: 46,
+    paddingBottom: 22,
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    backgroundColor: 'white',
+    zIndex: 1,
+  },
+});
 
 export default Settings;
